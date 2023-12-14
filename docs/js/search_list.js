ad6d1385ac5dc12b322415c8d2d7a6e2250af843
NGO-079 <146411811+NGO-079@users.noreply.github.com>
2023-12-14 12:54:37 +0900

    Update search_list.js

--- docs/js/search_list.js
+++ docs/js/search_list.js
@@ -1,6 +1,7 @@
 window.onload = function onLoad() {
 
     const search_btn = document.getElementById("search_btn");
+    
     search_btn.addEventListener("click", function(event){
         const keyword = document.getElementById("search_keyword").value;//id=search_keywordのinputタグからキーワードを取得
 
@@ -14,5 +15,38 @@ window.onload = function onLoad() {
         window.location.href = search_url; //検索機能の付いたhtmlとreadJSON_list.jsのコピーのペアに結果を表示させる
         window.open(search_url); //検索機能の付いたhtmlとreadJSON_list.jsのコピーのペアに結果を表示させる（別タブで開く）
     });
+    fetch_json(json_url).then(function(data){ //json_urlで読み出せるJSONデータ(data)の処理を行う
+		//console.log(data); //デバッグ用に取得したJSONデータをコンソールに表示
+		//ページ全体のタイトルなどの設定が必要であれば行う
+		const title = document.getElementsByTagName("title"); //titleタグを取得
+		const top_title = document.getElementById("top-title"); //ページの一番上の見出しの<div id="top-title">ところを取得
+		const top_abstract = document.getElementById("top-abstract"); //ページの一番上の見出しの<div id="top-abstract">ところを取得
+
+		let num=0; //項目の数を数える
+		//JSONデータから繰り返し内容部分のHTMLを繰り返し生成
+		for(let i=0; i<data.introduction_obj_list.length; i++){
+			let elem = data.introduction_obj_list[i]; //JSONデータから１つの項目を取り出す
+			let item_html='';
+
+			//?q=検索語 を付けてelemの要素を検索する場合は，下記のif文を入れる（item_htmlの生成の文を囲む）
+			if(q == 0 || (q != 0 && elem.title.indexOf(q) != -1)) { //?q=が無いときはq==0，?q=があるとき(q!=0)はelemの要素（title, abstract, detailなど）にマッチ
+			item_html += '<div class="col">';
+			item_html += '<div class="card">'; //Bootstrapのcardを使って繰り返し要素を出力する（ここでは1段のみ）
+			item_html += '<img class="card-img-top" src="photos/'+elem.image_file+'_thum.jpg" alt="'+elem.title+'の画像">'; //image_fileの値と対応する画像のファイル名に_thumを付けた.jpgファイルを用意する
+			item_html += '<div class="card-body">';
+			item_html += '<h5 class="card-title">'+elem.title+'</h5>';
+			item_html += '<p class="card-text">'+elem.abstract+'</p>';
+			item_html += '<a href="'+detail_html+'?id='+elem.id+'" class="card-link">詳細はこちらをクリック！</a>';
+			item_html += '</div>';
+			item_html += '</div>';
+			item_html += '</div>';
+			} //if(elem.title.indexOf(q) != -1) {の終わり（先頭の//だけ消す）
+			//”item_htmlの生成の文”はここまで
+
+			list_container_row.innerHTML += item_html;//生成したHTMLを<div id="main_content">～</div>間に追加
+			num++;
+		}
+	});
+
 
 };
